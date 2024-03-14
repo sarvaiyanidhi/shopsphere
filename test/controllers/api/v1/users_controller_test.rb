e5291f87176860c198f383fedcee229d8c2cf958
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2024-03-13 23:18:47 -0400

    User creation endpoint

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -5,6 +5,20 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     @user = users(:one)
   end
 
+  test "Should create user" do
+    assert_difference('User.count') do
+      post api_v1_users_url, params: { user: { email: "test@test.org", password: "123456" } }, as: :json
+    end
+    assert_response :created
+  end
+
+  test "Should not create user with taken email" do
+    assert_no_difference('User.count') do
+      post api_v1_users_url, params: { user: { email: @user.email, password: "123456" } }, as: :json
+    end
+    assert_response :unprocessable_entity
+  end
+
   test "Should show user" do
     get api_v1_user_url(@user), as: :json
     assert_response :success
