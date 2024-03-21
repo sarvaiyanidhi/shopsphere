437a135c24753e51ead5d676acc02e66ad38b538
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2024-03-20 23:18:39 -0400

    Included Authenticable module to handle authentication methods

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -8,6 +8,6 @@ class JsonWebToken
 
   def self.decode(token)
     decoded = JWT.decode(token, SECRET_KEY).first
-    HashWithIndfferentAccess.new decoded
+    HashWithIndifferentAccess.new decoded
   end
 end
