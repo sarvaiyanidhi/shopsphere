79eee6b4463bdf62d12b3e23678587bd8d9b5343
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2024-04-03 23:55:04 -0400

    Fix test failure issue

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -1,6 +1,6 @@
 module Authenticable
   def current_user
-    return current_user if @current_user
+    return @current_user if @current_user
 
     header = request.headers['Authorization']
     return nil if header.nil?
@@ -10,6 +10,8 @@ module Authenticable
     @current_user = User.find(decoded[:user_id]) rescue ActiveRecord::RecordNotFound
   end
 
+  protected
+  
   def check_login
     head :forbidden unless self.current_user
   end
