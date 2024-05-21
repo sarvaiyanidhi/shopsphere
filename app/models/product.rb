f0531eb34507fd78fb9f7f2e12b0b5690e8748c0
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2024-05-20 22:52:14 -0400

    Add price search for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,9 @@ class Product < ApplicationRecord
   scope :filter_by_title, lambda { |keyword|
         where('lower(title) LIKE ?', "%#{keyword.downcase}%")
         }
+
+  scope :above_or_equal_to_price, lambda { |price|
+        where('price >= ?', price)
+        }
+
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -14,4 +14,8 @@ class ProductTest < ActiveSupport::TestCase
   test 'should filter products by title and sort them' do
     assert_equal [products(:another_tv), products(:one)], Product.filter_by_title('tv').sort
   end
+
+  test 'should filter products by price and sort them' do
+    assert_equal [products(:two), products(:one)], Product.above_or_equal_to_price(200).sort
+  end
 end
